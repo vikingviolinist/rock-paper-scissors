8ddabd73edcca94b5e8d6f86ae94ad8236b60386
Justine Lester Cruz <justinelester.cruz@gmail.com>
2018-04-07 23:15:45 +0800

    Divide game() function to smaller functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,81 @@
-"use strict";
+'use strict';
+
+function playerPlay() {
+  let playerSelection;
+
+  while (!(playerSelection === 'rock' || playerSelection === 'paper' ||
+      playerSelection === 'scissors')) {
+    playerSelection = prompt('Rock, paper, or scissors? Type your choice.');
+    playerSelection = playerSelection.toLowerCase();
+  }
+
+  return playerSelection;
+}
 
 function computerPlay() {
   const choiceNumber = Math.floor(Math.random() * 3);
 
   switch (choiceNumber) {
     case 0:
-      return "rock";
+      return 'rock';
       break;
     case 1:
-      return "paper";
+      return 'paper';
       break;
     case 2:
-      return "scissors";
+      return 'scissors';
       break;
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "scissors" ||
-      playerSelection === "paper" && computerSelection === "rock" ||
-      playerSelection === "scissors" && computerSelection === "paper") {
+  if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+      playerSelection === 'paper' && computerSelection === 'rock' ||
+      playerSelection === 'scissors' && computerSelection === 'paper') {
     console.log(`You win, for ${playerSelection} beats ${computerSelection}.`);
-    return "win";
-  } else if (computerSelection === "rock" && playerSelection === "scissors" ||
-      computerSelection === "paper" && playerSelection === "rock" ||
-      computerSelection === "scissors" && playerSelection === "paper") {
+    return 'win';
+  } else if (computerSelection === 'rock' && playerSelection === 'scissors' ||
+      computerSelection === 'paper' && playerSelection === 'rock' ||
+      computerSelection === 'scissors' && playerSelection === 'paper') {
     console.log(`You lose, for ${computerSelection} beats ${playerSelection}.`);
-    return "lose";
+    return 'lose';
   } else {
     console.log(`It's a tie, for you both chose ${playerSelection}.`);
-    return "tie";
+    return 'tie';
+  }
+}
+
+function checkWinner(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    console.log('');
+    console.log('CONGRATULATIONS! YOU WON.');
+  } else if (computerScore > playerScore) {
+    console.log('');
+    console.log('SORRY! YOU LOST.');
+  } else {
+    console.log('')
+    console.log('WOW! IT\'S A DRAW.');
   }
 }
 
 function game() {
   let playerScore = 0;
   let computerScore = 0;
+  let roundCount = 0;
 
-  for (let i = 0; i < 5; i++) {
-    console.log("");
-    console.log(`ROUND ${i + 1}`);
+  while (playerScore < 5 && computerScore < 5) {
+    roundCount++;
+    
+    console.log('');
+    console.log(`ROUND ${roundCount}`);
 
-    let playerSelection = prompt("Rock, paper, or scissors? Type your choice.");
-    playerSelection = playerSelection.toLowerCase();
-
-    while (!(playerSelection === "rock" || playerSelection === "paper" ||
-        playerSelection === "scissors")) {
-      playerSelection = prompt("Rock, paper, or scissors? Type your choice.");
-      playerSelection = playerSelection.toLowerCase();
-    }
-
+    const playerSelection = playerPlay();
     const computerSelection = computerPlay();
     const roundResult = playRound(playerSelection, computerSelection);
 
-    if (roundResult === "win") {
+    if (roundResult === 'win') {
       playerScore++;
-    } else if (roundResult === "lose") {
+    } else if (roundResult === 'lose') {
       computerScore++;
     }
 
@@ -63,16 +83,8 @@ function game() {
     console.log(`Computer: ${computerScore}`);
   }
 
-  if (playerScore > computerScore) {
-    console.log("");
-    console.log("CONGRATULATIONS! YOU WON.");
-  } else if (computerScore > playerScore) {
-    console.log("");
-    console.log("SORRY! YOU LOST.");
-  } else {
-    console.log("WOW! IT'S A DRAW.");
-  }
+  checkWinner(playerScore, computerScore);
 
-  console.log("");
-  console.log("Type \"game();\" (without quotes) to play again.");
+  console.log('');
+  console.log('Type "game();" (without quotes) to play again.');
 }
